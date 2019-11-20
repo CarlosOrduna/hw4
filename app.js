044cd87e838dfec89e64a24785932b3c0459312d
EC2 Default User <ec2-user@ip-172-31-18-143.us-west-2.compute.internal>
2019-11-20 05:42:05 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+var fakeData = require("faker");
+ 
+//routes
+app.get("/", function(req, res) {
+    res.render("index.ejs", {randomName:fakeData.name.findName()});
+});
+
+app.get("/index", function(req, res) {
+    res.render("index.ejs", {randomName:fakeData.name.findName()});
+});
+
+app.get("/security",function(req,res){
+    res.render("security.ejs", {randomName:fakeData.name.findName()});
+});
+
+app.get("/networking",function(req,res){
+    res.render("networking.ejs", {randomName:fakeData.name.findName()});
+});
+
+app.get("/cryptography",function(req,res){
+    res.render("cryptography.ejs", {randomName:fakeData.name.findName()});
+});
+
+
+// //server listener
+// app.listen("8080","127.0.0.1", function(){
+//     console.log("Express Server is Running...");
+// });
+
+//heroku listener
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Running Express Server...");
+});
